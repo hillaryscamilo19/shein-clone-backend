2cb8f79bb398c9923254f0fdcd43d817b4af4d15
hillaryscamilo19 <hcr090120@gmail.com>
2022-09-05 16:45:02 -0400

    dat

---
+++ dat.js
@@ -0,0 +1,90 @@
+const data = [
+    {
+        "object": "conjunto",
+        "price": 650,
+        "img": "https://i.pinimg.com/564x/13/18/b3/1318b3204b7038b5efc363284ad40a7c.jpg",
+        "title": "Brown and white set",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 588,
+        "img": "https://i.pinimg.com/564x/01/ad/31/01ad31e607f27ef990e973294721da97.jpg",
+        "title": "multicolor shirt",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 820,
+        "img": "https://i.pinimg.com/564x/36/c6/06/36c6066ff6f34a4f3029de31725bd077.jpg",
+        "title": "Blue shirt",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 1500,
+        "img": "https://i.pinimg.com/564x/12/c9/6b/12c96bb574588d407fdaf02f3cfd4f79.jpg",
+        "title": "yellow dress",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 900,
+        "img": "https://i.pinimg.com/564x/a2/97/30/a29730d737fd2d96af2955f3e3d2f8b2.jpg",
+        "title": "simple red dress",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 800,
+        "img": "https://i.pinimg.com/564x/32/0d/c6/320dc693b2d0008729295931be4f6ae5.jpg",
+        "title": "Simple yellow dress ",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 800,
+        "img": "https://i.pinimg.com/564x/2c/41/59/2c4159b37a08a1574a62e111c828da2f.jpg",
+        "title": "Outfit",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 800,
+        "img": "https://i.pinimg.com/564x/2c/41/59/2c4159b37a08a1574a62e111c828da2f.jpg",
+        "title": "Outfit",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 800,
+        "img": "https://i.pinimg.com/564x/2c/41/59/2c4159b37a08a1574a62e111c828da2f.jpg",
+        "title": "Outfit",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 800,
+        "img": "https://i.pinimg.com/564x/2c/41/59/2c4159b37a08a1574a62e111c828da2f.jpg",
+        "title": "Outfit",
+        "quantity":"1",
+        "classification": "kids"
+    },
+    {
+        "object": "conjunto",
+        "price": 800,
+        "img": "https://i.pinimg.com/564x/2c/41/59/2c4159b37a08a1574a62e111c828da2f.jpg",
+        "title": "Outfit",
+        "quantity":"1",
+        "classification": "kids"
+    },
+]
